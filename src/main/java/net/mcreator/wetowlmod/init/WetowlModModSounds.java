59ee973dc55ce5357a378f788ed1d57fd38e42d5
Eienrojin <Stkchanelx@gmail.com>
2022-05-28 00:04:42 +0300

    test commit

---
+++ src/main/java/net/mcreator/wetowlmod/init/WetowlModModSounds.java
@@ -0,0 +1,29 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.wetowlmod.init;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class WetowlModModSounds {
+	public static Map<ResourceLocation, SoundEvent> REGISTRY = new HashMap<>();
+	static {
+		REGISTRY.put(new ResourceLocation("wetowl_mod", "rhodes_lofi"), new SoundEvent(new ResourceLocation("wetowl_mod", "rhodes_lofi")));
+	}
+
+	@SubscribeEvent
+	public static void registerSounds(RegistryEvent.Register<SoundEvent> event) {
+		for (Map.Entry<ResourceLocation, SoundEvent> sound : REGISTRY.entrySet())
+			event.getRegistry().register(sound.getValue().setRegistryName(sound.getKey()));
+	}
+}
