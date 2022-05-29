b1ab6a75962674fc7a3a2c8db98808acebf0cf46
Nikita Polunin (二木立・半人) <77877550+Eienrojin@users.noreply.github.com>
2022-05-29 18:01:44 +0300

    Update RatRenderer.java

--- src/main/java/net/mcreator/wetowlmod/client/renderer/RatRenderer.java
+++ src/main/java/net/mcreator/wetowlmod/client/renderer/RatRenderer.java
@@ -1,6 +1,13 @@
 
 package net.mcreator.wetowlmod.client.renderer;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+
+import net.mcreator.wetowlmod.entity.RatEntity;
+import net.mcreator.wetowlmod.client.model.Modelrat;
+
 public class RatRenderer extends MobRenderer<RatEntity, Modelrat<RatEntity>> {
 
 	public RatRenderer(EntityRendererProvider.Context context) {
