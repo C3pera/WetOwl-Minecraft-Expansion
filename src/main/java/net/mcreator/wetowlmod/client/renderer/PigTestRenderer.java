efb53aa6f2f4b4d3b796725d26ca31e02ea8c670
Nikita Polunin (二木立・半人) <77877550+Eienrojin@users.noreply.github.com>
2022-05-29 18:01:14 +0300

    Update PigTestRenderer.java

--- src/main/java/net/mcreator/wetowlmod/client/renderer/PigTestRenderer.java
+++ src/main/java/net/mcreator/wetowlmod/client/renderer/PigTestRenderer.java
@@ -1,6 +1,14 @@
 
 package net.mcreator.wetowlmod.client.renderer;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.PigModel;
+
+import net.mcreator.wetowlmod.entity.PigTestEntity;
+
 public class PigTestRenderer extends MobRenderer<PigTestEntity, PigModel<PigTestEntity>> {
 
 	public PigTestRenderer(EntityRendererProvider.Context context) {
