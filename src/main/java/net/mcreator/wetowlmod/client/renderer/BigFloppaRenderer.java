148a8c883e667e2151a1d7bfae5de87bf89cd5f0
Nikita Polunin (二木立・半人) <77877550+Eienrojin@users.noreply.github.com>
2022-05-29 17:58:24 +0300

    Update BigFloppaRenderer.java

--- src/main/java/net/mcreator/wetowlmod/client/renderer/BigFloppaRenderer.java
+++ src/main/java/net/mcreator/wetowlmod/client/renderer/BigFloppaRenderer.java
@@ -1,6 +1,13 @@
 
 package net.mcreator.wetowlmod.client.renderer;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+
+import net.mcreator.wetowlmod.entity.BigFloppaEntity;
+import net.mcreator.wetowlmod.client.model.Modelbigfloppa;
+
 public class BigFloppaRenderer extends MobRenderer<BigFloppaEntity, Modelbigfloppa<BigFloppaEntity>> {
 
 	public BigFloppaRenderer(EntityRendererProvider.Context context) {
